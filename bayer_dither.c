da6a7f93ddb0df590401cf3df1979230209139af
yjeum11 <yjeum11@gmail.com>
2026-03-13 11:30:45 +0000

    added general size bayer matrix and gamma correction

--- bayer_dither.c
+++ bayer_dither.c
@@ -10,25 +10,74 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-void bayer_4x4(unsigned char *gray_data, int x, int y, int invert) {
-    unsigned char bayer_matrix[16] = {
-        0, 8, 2, 10,
-        12, 4, 14, 6,
-        3, 11, 1, 9,
-        15, 7, 13, 5
-    };
+void gamma_correct(unsigned char *input, int x, int y) {
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            int idx = i * x + j;
+            float scaled = input[idx] / 255.0;
+            scaled = powf(scaled, 2.2);
+            scaled *= 255.0;
+            input[idx] = (unsigned char)scaled;
+        }
+    }
+}
+
+unsigned char *bayer_matrix_gen(int k) {
+    if (k <= 1) {
+        unsigned char *result = malloc(4);
+        unsigned char temp[4] = {0, 2, 3, 1};
+        memcpy(result, temp, 4);
+        return result;
+    }
+    unsigned char *prev = bayer_matrix_gen(k-1);
+    // level 1 is 2x2 
+    // level 2 is 4x4
+    // ... level n is 2^n x 2^n
+    unsigned int size = 1 << k;
+    unsigned int prev_size = 1 << (k-1);
+
+    unsigned char *result = malloc(size*size);
+    for (unsigned int i = 0; i < size; i++) {
+        for (unsigned int j = 0; j < size; j++) {
+            unsigned int idx = i * size + j;
+            unsigned int prev_idx = (i % prev_size) * prev_size + (j % prev_size);
+
+            if (i < prev_size) {
+                if (j < prev_size) {
+                    result[idx] = 4 * prev[prev_idx];
+                } else {
+                    result[idx] = 4 * prev[prev_idx] + 2;
+                }
+            } else {
+                if (j < prev_size) {
+                    result[idx] = 4 * prev[prev_idx] + 3;
+                } else {
+                    result[idx] = 4 * prev[prev_idx] + 1;
+                }
+            }
+        }
+    }
+
+    free(prev);
+    return result;
+}
+
+void bayer(unsigned char *gray_data, int x, int y, int level, int invert) {
+    unsigned char *bayer_matrix = bayer_matrix_gen(level);
+    int bayer_size = 1 << level;
+    printf("bayer_szie = %d\n", bayer_size);
 
     // Normalize bayer_matrix to 0-255
-    for (int i = 0; i < 16; ++i) {
-        bayer_matrix[i] = bayer_matrix[i] * (256 / 16); 
+    for (int i = 0; i < (1 << (level*2)); ++i) {
+        bayer_matrix[i] = bayer_matrix[i] * (256 / (1 << (level*2))); 
     }
 
     for (int i = 0; i < y; ++i) {
         for (int j = 0; j < x; ++j) {
             int pixel_idx = i * x + j;
             int bayer_threshold = bayer_matrix[
-                (j % 4) * 4 +
-                i % 4
+                (i % bayer_size) * bayer_size +
+                j % bayer_size
             ];
             if (invert)
                 bayer_threshold = 256 - bayer_threshold;
@@ -36,14 +85,17 @@ void bayer_4x4(unsigned char *gray_data, int x, int y, int invert) {
         }
     }
 
+    free(bayer_matrix);
+
 }
 
 int main(int argc, char *argv[]) {
     char opt;
     char *infile = NULL;
     char *outfile = NULL;
+    int level = 1;
     int invert = 0;
-    while ((opt = getopt(argc, argv, "i:o:v")) != -1) {
+    while ((opt = getopt(argc, argv, "i:o:vl:")) != -1) {
         if (opt == 'i') {
             infile = optarg;
         }
@@ -52,6 +104,9 @@ int main(int argc, char *argv[]) {
         }
         if (opt == 'v') {
             invert = 1;
+        }
+        if (opt == 'l') {
+            level = atoi(optarg);
         }
     }
 
@@ -73,7 +128,9 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    bayer_4x4(image_data, image_x, image_y, invert);
+    gamma_correct(image_data, image_x, image_y);
+
+    bayer(image_data, image_x, image_y, level, invert);
 
     stbi_write_png(outfile, image_x, image_y, 1, image_data, image_x);
 
